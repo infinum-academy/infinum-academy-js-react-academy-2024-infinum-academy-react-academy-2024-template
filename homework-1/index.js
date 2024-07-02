b243783ba9da511d85f0b1b38632ce2bab7201d8
Luka Vukovic <vukovicluka33@gmail.com>
2024-07-02 22:39:16 +0200

    Add functional delete button and average rating
    Delete button removes exact element and updates UI.

--- homework-1/index.js
+++ homework-1/index.js
@@ -1,14 +1,18 @@
 const reviewList = document.querySelector(".review-list");
-const reviewArr = JSON.parse(localStorage.getItem("review-array")) || [];
+const averageRatingEl = document.querySelector(".average-rating");
+let reviewArr = JSON.parse(localStorage.getItem("review-array")) || [];
 
 function renderReviews(r) {
-  if (!reviewList.childElementCount) {
-    reviewArr.forEach((review) => {
+  if (r) {
+    reviewList.appendChild(createReviewElement(r));
+  } else {
+    reviewArr.forEach(review => {
       reviewList.appendChild(createReviewElement(review));
     });
-  } else if (r) {
-    reviewList.appendChild(createReviewElement(r));
   }
+  const totalRating = reviewArr.reduce((sum, {rating}) => sum + parseInt(rating), 0);
+  const averageRating = reviewArr.length > 0 ? totalRating / reviewArr.length : 0;
+  averageRatingEl.innerHTML = `${averageRating.toFixed(2)} (${reviewArr.length})`;
 }
 
 function createReviewElement(review) {
@@ -16,8 +20,13 @@ function createReviewElement(review) {
 
   reviewEl.classList.add("review-element");
   reviewEl.innerHTML = `
-    <p>${review.comment}</p>
-    <p>${review.rating} / 5</p>
+    <div>
+      <p>${review.comment}</p>
+      <p>${review.rating} / 5</p>
+    </div>
+    <button class="review-delete-btn" onclick='deleteReview(${JSON.stringify(
+      review
+    )})'>Delete</button>
   `;
   return reviewEl;
 }
@@ -25,11 +34,17 @@ function createReviewElement(review) {
 function addNewReview() {
   const comment = document.getElementById("review-comment");
   const rating = document.getElementById("review-rating");
+  let id;
 
   if (!comment.value || !rating.value || rating.value < 1 || rating.value > 5)
     return;
 
+  if(reviewArr.length){
+    id = reviewArr[reviewArr.length - 1].id + 1;
+  }
+  
   const newReview = {
+    id: id || 1,
     comment: comment.value,
     rating: rating.value,
   };
@@ -38,6 +53,13 @@ function addNewReview() {
   renderReviews(newReview);
   comment.value = "";
   rating.value = "";
+}
+
+function deleteReview(review) {
+  reviewArr = reviewArr.filter((r) => r.id != review.id);
+  localStorage.setItem("review-array", JSON.stringify(reviewArr));
+  reviewList.innerHTML = "";
+  renderReviews();
 }
 
 renderReviews();
