bf1f9d9f828651fa1a23b47668f6f3c6c6abc448
Luka Vukovic <vukovicluka33@gmail.com>
2024-07-03 22:47:41 +0200

    Enhance rating selection by using star icons
    Review rating can be selected by clicking on star icon which represents
    1-5 rating. Stars are styled on hover and on selection. Accessibility is also
    improved by ability to enter form values using tabs or arrow keys.
    Deleting specific review from review list is optimized.
    Previous functionalities such as reseting form inputs are preserved.

--- homework-1/index.js
+++ homework-1/index.js
@@ -1,28 +1,40 @@
 const reviewList = document.querySelector(".review-list");
 const averageRatingEl = document.querySelector(".average-rating");
 let reviewArr = JSON.parse(localStorage.getItem("review-array")) || [];
+let rating;
 
-function renderReviews(r) {
-  if (r) {
-    reviewList.appendChild(createReviewElement(r));
+function renderReviews(newReview) {
+  if (newReview) {
+    reviewList.insertBefore(
+      createReviewElement(newReview),
+      reviewList.firstChild
+    );
+    styleRatingStars(newReview.rating, newReview.id);
   } else {
-    reviewArr.forEach(review => {
+    reviewArr.forEach((review) => {
       reviewList.appendChild(createReviewElement(review));
+      styleRatingStars(review.rating, review.id);
     });
   }
-  const totalRating = reviewArr.reduce((sum, {rating}) => sum + parseInt(rating), 0);
-  const averageRating = reviewArr.length > 0 ? totalRating / reviewArr.length : 0;
-  averageRatingEl.innerHTML = `${averageRating.toFixed(2)} (${reviewArr.length})`;
+  calculateAverageRating();
 }
 
 function createReviewElement(review) {
   const reviewEl = document.createElement("div");
+  reviewEl.setAttribute("data-index", review.id);
 
   reviewEl.classList.add("review-element");
   reviewEl.innerHTML = `
     <div>
       <p>${review.comment}</p>
       <p>${review.rating} / 5</p>
+      <p data-rating-index=${review.id} class="review-rated">
+        <i class="fa-regular fa-star star"></i>
+        <i class="fa-regular fa-star star"></i>
+        <i class="fa-regular fa-star star"></i>
+        <i class="fa-regular fa-star star"></i>
+        <i class="fa-regular fa-star star"></i>
+      </p>
     </div>
     <button class="review-delete-btn" onclick='deleteReview(${JSON.stringify(
       review
@@ -33,33 +45,77 @@ function createReviewElement(review) {
 
 function addNewReview() {
   const comment = document.getElementById("review-comment");
-  const rating = document.getElementById("review-rating");
   let id;
 
-  if (!comment.value || !rating.value || rating.value < 1 || rating.value > 5)
-    return;
+  if (!comment.value || !rating || rating < 1 || rating > 5) return;
 
-  if(reviewArr.length){
-    id = reviewArr[reviewArr.length - 1].id + 1;
+  if (reviewArr.length) {
+    id = reviewArr[0].id + 1;
   }
-  
+
   const newReview = {
     id: id || 1,
     comment: comment.value,
-    rating: rating.value,
+    rating: rating,
   };
-  reviewArr.push(newReview);
+  reviewArr.unshift(newReview);
   localStorage.setItem("review-array", JSON.stringify(reviewArr));
   renderReviews(newReview);
-  comment.value = "";
-  rating.value = "";
+  resetFormInputs();
 }
 
 function deleteReview(review) {
+  const deletedElement = document.querySelector(`[data-index="${review.id}"]`);
+  deletedElement.remove();
   reviewArr = reviewArr.filter((r) => r.id != review.id);
+
   localStorage.setItem("review-array", JSON.stringify(reviewArr));
-  reviewList.innerHTML = "";
-  renderReviews();
+  if (!reviewArr.length) {
+    localStorage.removeItem("review-array");
+  }
+  calculateAverageRating();
+}
+
+function onRatingChange(event) {
+  rating = event.target.value;
+  styleRatingStars(rating);
+}
+
+function resetFormInputs() {
+  rating = null;
+  const stars = document.querySelectorAll(".review-rating i");
+  stars.forEach((star) => (star.style.color = "#fff"));
+
+  const comment = document.getElementById("review-comment");
+  comment.value = "";
+}
+
+function calculateAverageRating() {
+  const totalRating = reviewArr.reduce(
+    (sum, { rating }) => sum + parseInt(rating),
+    0
+  );
+  const averageRating =
+    reviewArr.length > 0 ? totalRating / reviewArr.length : 0;
+  averageRatingEl.innerHTML = `${averageRating.toFixed(2)} (${
+    reviewArr.length
+  })`;
+}
+
+function styleRatingStars(rating, id) {
+  let stars;
+  if (id) {
+    stars = document.querySelectorAll(`[data-rating-index="${id}"] i`);
+  } else {
+    stars = document.querySelectorAll(".review-rating i");
+  }
+  const selectedIndex = 5 - rating;
+
+  stars.forEach((star) => (star.style.color = "#fff"));
+
+  for (let i = 4; i >= selectedIndex; i--) {
+    stars[i].style.color = "gold";
+  }
 }
 
 renderReviews();
